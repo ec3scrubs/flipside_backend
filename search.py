227f4c7961c8b139c928a3419336566e881fb4de
MX Chai <mingxuan@live.com.sg>
2016-05-15 08:35:32 +0800

    Restructured search to accomodate new query structure

--- search.py
+++ search.py
@@ -37,97 +37,105 @@ def build_dict(input_dict_file):
     dict_file.close()
     return (dictionary, document_length)
 
-def execute_queries(input_post_file, input_query_file, output_file, dictionary, document_length):
+def execute_queries(input_post_file, query, dictionary, document_length):
     """
     Tests the queries in the input_query_file based on the dictionary and postings.
     Writes results into output_file.
     """   
 
-    # Initialisation
-    queries = file(input_query_file, 'r')   
+    # Initialisation  
     postings = file(input_post_file, 'r')
-    output = file(output_file, 'w')
     stemmer = nltk.stem.porter.PorterStemmer()
 
     # Dictionary to store all the scores
     table = {}
 
-    # Reads the query line by line    
-    for query in queries.readlines():
-        terms = query.strip().split(" ")
-        table["query"] = {}
-        table["idf"] = {}
+    # Reads the query   
+    terms = query.strip().split(" ")
+    table["query"] = {}
+    table["idf"] = {}
 
-        # Scheme: lnc.ltc
-        for term in terms:
-            term = stemmer.stem(term).lower()
+    # Scheme: lnc.ltc
+    for term in terms:
+        term = stemmer.stem(term).lower()
 
-            # Update tf in "query" dictionary
-            if term not in table["query"]:
-                table["query"][term] = 1
-            else:
-                table["query"][term] += 1 
+        # Update tf in "query" dictionary
+        if term not in table["query"]:
+            table["query"][term] = 1
+        else:
+            table["query"][term] += 1 
 
-            # Fix
-            if term not in dictionary:
-                continue
-            
-            # Calculate idf of this term
-            # Optimised against query such as "cat cat cat cat" to avoid 
-            # repeated calculation of idf of the same term
-            # Will just fetch table["idf"]["cat"]
-            if term not in table["idf"]:
-                document_freq = dictionary[term][1]
-                idf = math.log((float(document_length)/document_freq), 10)
-                table["idf"][term] = idf # store the idf of this term into the dictionary
-
-            # Read the NORMALIZED wt (read from the postings file) of the term in every document
-            byte_offset = dictionary[term][0]
-            posting_reader = PostingReader(postings, byte_offset)  
-            containing_docs = posting_reader.to_list()
-
-            # Build the rows
-            for (doc_id, freq) in containing_docs:
-                if doc_id not in table:
-                    table[doc_id] = {}
-                table[doc_id][term] = freq     
-            
-        # Normalising for documents can only be done after every term has been looked at
-        # Same goes for the calculation of score
-        # Do the normalising here
-
-        # Overwrite the values with the tf-idf weight for query (the values used to be raw tf)
-        for (term, freq) in table["query"].items():
-            if term in dictionary:
-                table["query"][term] = (1+math.log(table["query"][term], 10)) * table["idf"][term]
-
-        # Construct normalised weight for query column
-        unit_length = reduce(lambda x, y: x+y, map(lambda x: x[1]**2, table["query"].items()))
-        unit_length = math.sqrt(unit_length)
-        for (term, freq) in table["query"].items():
-            # Overwrite the values with the normalised weight
-            table["query"][term] = table["query"][term] / unit_length
+        # Fix
+        if term not in dictionary:
+            continue
         
-        # Construct (doc_id, all query terms) score
-        all_doc_ids = table.keys()
-        all_doc_ids.remove("idf")
-        all_doc_ids.remove("query")
+        # Calculate idf of this term
+        # Optimised against query such as "cat cat cat cat" to avoid 
+        # repeated calculation of idf of the same term
+        # Will just fetch table["idf"]["cat"]
+        if term not in table["idf"]:
+            document_freq = dictionary[term][1]
+            idf = math.log((float(document_length)/document_freq), 10)
+            table["idf"][term] = idf # store the idf of this term into the dictionary
 
-        doc_score = {}
-        for doc_id in all_doc_ids:
-            doc_score[doc_id] = 0
+        # Read the NORMALIZED wt (read from the postings file) of the term in every document
+        byte_offset = dictionary[term][0]
+        posting_reader = PostingReader(postings, byte_offset)  
+        containing_docs = posting_reader.to_list()
 
-        for (term, wt) in table["query"].items():
-            for doc_id in all_doc_ids:
-                if term in table[doc_id]: # if the ith term of the query is present in the document
-                    doc_score[doc_id] += table[doc_id][term] * wt # table[doc_id][term] contains the normalised wt for term
+        # Build the rows
+        for (doc_id, freq) in containing_docs:
+            if doc_id not in table:
+                table[doc_id] = {}
+            table[doc_id][term] = freq     
+        
+    # Normalising for documents can only be done after every term has been looked at
+    # Same goes for the calculation of score
+    # Do the normalising here
 
-        # print sorted(doc_score.items(), key=lambda x: (x[1], -x[0]), reverse=True)[:10]
-        top_10 = map(lambda x: x[0], filter(lambda x: x[1] > 0, sorted(doc_score.items(), key=lambda x: (x[1], -x[0]), reverse=True)[:10]))
+    # Overwrite the values with the tf-idf weight for query (the values used to be raw tf)
+    for (term, freq) in table["query"].items():
+        if term in dictionary:
+            table["query"][term] = (1+math.log(table["query"][term], 10)) * table["idf"][term]
+
+    # Construct normalised weight for query column
+    unit_length = reduce(lambda x, y: x+y, map(lambda x: x[1]**2, table["query"].items()))
+    unit_length = math.sqrt(unit_length)
+    for (term, freq) in table["query"].items():
+        # Overwrite the values with the normalised weight
+        table["query"][term] = table["query"][term] / unit_length
     
-        # Write to file
-        output_line = reduce(lambda x, y: x + str(y) + " ", top_10, "").strip() + "\n"
-        output.write(output_line)
+    # Construct (doc_id, all query terms) score
+    all_doc_ids = table.keys()
+    all_doc_ids.remove("idf")
+    all_doc_ids.remove("query")
+
+    doc_score = {}
+    for doc_id in all_doc_ids:
+        doc_score[doc_id] = 0
+
+    for (term, wt) in table["query"].items():
+        for doc_id in all_doc_ids:
+            if term in table[doc_id]: # if the ith term of the query is present in the document
+                doc_score[doc_id] += table[doc_id][term] * wt # table[doc_id][term] contains the normalised wt for term
+
+    # print sorted(doc_score.items(), key=lambda x: (x[1], -x[0]), reverse=True)[:10]
+    top_10 = map(lambda x: x[0], filter(lambda x: x[1] > 0, sorted(doc_score.items(), key=lambda x: (x[1], -x[0]), reverse=True)[:10]))
+    stars = get_stars(top_10) # returns a dictionary
+    return stars
+
+    # Write to file
+    output_line = reduce(lambda x, y: x + str(y) + " ", top_10, "").strip() + "\n"
+    output.write(output_line)
+
+# Quick hack to get the stars of each review, should have done this indexing but I'm lazy
+def get_stars(doc_list):
+    stars = {}
+    for doc in doc_list:
+        f = file("dl/output/" + str(doc), "r")
+        star = f.readline().strip()
+        stars[doc] = star
+    return stars
 
 class PostingReader:
     def __init__(self, postings_file, byte_offset):
@@ -162,30 +170,40 @@ class PostingReader:
         # print output
         return output
 
-def usage():
-    print "usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results"
+# def usage():
+#     print "usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results"
 
-input_dict_file = input_post_file = input_query_file = output_file = None
-try:
-    opts, args = getopt.getopt(sys.argv[1:], 'd:p:q:o:')
-except getopt.GetoptError, err:
-    usage()
-    sys.exit(2)
-for o, a in opts:
-    if o == '-d':
-        input_dict_file = a
-    elif o == '-p':
-        input_post_file = a
-    elif o == '-q':
-        input_query_file = a
-    elif o == '-o':
-        output_file = a
-    else:
-        assert False, "unhandled option"
-if input_dict_file == None or input_post_file == None or input_query_file == None or output_file == None:
-    usage()
-    sys.exit(2)
+# input_dict_file = input_post_file = input_query_file = output_file = None
+# try:
+#     opts, args = getopt.getopt(sys.argv[1:], 'd:p:q:o:')
+# except getopt.GetoptError, err:
+#     usage()
+#     sys.exit(2)
+# for o, a in opts:
+#     if o == '-d':
+#         input_dict_file = a
+#     elif o == '-p':
+#         input_post_file = a
+#     elif o == '-q':
+#         input_query_file = a
+#     elif o == '-o':
+#         output_file = a
+#     else:
+#         assert False, "unhandled option"
+# if input_dict_file == None or input_post_file == None or input_query_file == None or output_file == None:
+#     usage()
+#     sys.exit(2)
 
 # Execution
-(dictionary, document_length) = build_dict(input_dict_file)
-execute_queries(input_post_file, input_query_file, output_file, dictionary, document_length)
+def main(dictionary, posting, query):
+    input_dict_file = dictionary
+    input_post_file = posting
+    (dictionary, document_length) = build_dict(input_dict_file)
+    return execute_queries(input_post_file, query, dictionary, document_length)
+
+
+
+
+
+
+
